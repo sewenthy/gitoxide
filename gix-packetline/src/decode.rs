7f4287b527c7baebcfe81d2df492c51733476318
Sewen Thy <me@sewenthy.dev>
2023-02-22 12:33:49 +0800

    tmp

--- gix-packetline/src/decode.rs
+++ gix-packetline/src/decode.rs
@@ -72,7 +72,7 @@ pub fn hex_prefix(four_bytes: &[u8]) -> Result<PacketLineOrWantedSize<'_>, Error
     }
 
     let mut buf = [0u8; U16_HEX_BYTES / 2];
-    hex::decode_to_slice(four_bytes, &mut buf).map_err(|err| Error::HexDecode { err: err.to_string() })?;
+    hex::decode_to_slice(four_bytes, &mut buf).map_err(|err| Error::HexDecode { err: err.to_string() }).unwrap();
     let wanted_bytes = u16::from_be_bytes(buf);
 
     if wanted_bytes == 3 {
