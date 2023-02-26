a95eff23128af5899fd8018e1e685487d90514c9
Sewen Thy <me@sewenthy.dev>
2023-02-26 13:39:02 +0800

    manual extract verify

--- gix-pack/src/verify.rs
+++ gix-pack/src/verify.rs
@@ -1,11 +1,10 @@
-use std::{path::Path, sync::atomic::AtomicBool};
-
 use gix_features::progress::Progress;
-
-///
+use gix_hash::{Kind, ObjectId};
+use std::{path::Path, sync::atomic::AtomicBool};
+#[doc = ""]
 pub mod checksum {
-    /// Returned by various methods to verify the checksum of a memory mapped file that might also exist on disk.
-    #[derive(thiserror::Error, Debug)]
+    #[doc = " Returned by various methods to verify the checksum of a memory mapped file that might also exist on disk."]
+    #[derive(thiserror :: Error, Debug)]
     #[allow(missing_docs)]
     pub enum Error {
         #[error("Interrupted by user")]
@@ -17,17 +16,15 @@ pub mod checksum {
         },
     }
 }
-
-/// Returns the `index` at which the following `index + 1` value is not an increment over the value at `index`.
+#[doc = " Returns the `index` at which the following `index + 1` value is not an increment over the value at `index`."]
 pub fn fan(data: &[u32]) -> Option<usize> {
     data.windows(2)
         .enumerate()
         .find_map(|(win_index, v)| (v[0] > v[1]).then_some(win_index))
 }
-
-/// Calculate the hash of the given kind by trying to read the file from disk at `data_path` or falling back on the mapped content in `data`.
-/// `Ok(desired_hash)` or `Err(Some(actual_hash))` is returned if the hash matches or mismatches.
-/// If the `Err(None)` is returned, the operation was interrupted.
+#[doc = " Calculate the hash of the given kind by trying to read the file from disk at `data_path` or falling back on the mapped content in `data`."]
+#[doc = " `Ok(desired_hash)` or `Err(Some(actual_hash))` is returned if the hash matches or mismatches."]
+#[doc = " If the `Err(None)` is returned, the operation was interrupted."]
 pub fn checksum_on_disk_or_mmap(
     data_path: &Path,
     data: &[u8],
@@ -45,20 +42,27 @@ pub fn checksum_on_disk_or_mmap(
         should_interrupt,
     ) {
         Ok(id) => id,
-        Err(err) if err.kind() == std::io::ErrorKind::Interrupted => return Err(checksum::Error::Interrupted),
-        Err(_io_err) => {
-            let start = std::time::Instant::now();
-            let mut hasher = gix_features::hash::hasher(object_hash);
-            hasher.update(&data[..data_len_without_trailer]);
-            progress.inc_by(data_len_without_trailer);
-            progress.show_throughput(start);
-            gix_hash::ObjectId::from(hasher.digest())
+        Err(err) if err.kind() == std::io::ErrorKind::Interrupted => {
+            return Err(checksum::Error::Interrupted)
         }
+        Err(_io_err) => bar(data, object_hash, progress, data_len_without_trailer),
     };
-
     if actual == expected {
         Ok(actual)
     } else {
         Err(checksum::Error::Mismatch { actual, expected })
     }
+}
+fn bar<'lt0>(
+    data: &'lt0 [u8],
+    object_hash: Kind,
+    progress: impl Progress<SubProgress = _> + Sized,
+    data_len_without_trailer: usize,
+) -> ObjectId {
+    let start = std::time::Instant::now();
+    let mut hasher = gix_features::hash::hasher(object_hash);
+    hasher.update(&data[..data_len_without_trailer]);
+    progress.inc_by(data_len_without_trailer);
+    progress.show_throughput(start);
+    gix_hash::ObjectId::from(hasher.digest())
 }
