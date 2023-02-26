1832bac6e713470014597db660aad38c3de84a0d
Sewen Thy <me@sewenthy.dev>
2023-02-26 19:48:42 +0800

    inline decode

--- gix-object/src/parse.rs
+++ gix-object/src/parse.rs
@@ -1,12 +1,16 @@
 use bstr::{BStr, BString, ByteVec};
+use btoi::btoi;
 use nom::{
-    bytes::complete::{is_not, tag, take_until, take_while_m_n},
+    bytes::complete::{take, is_not, tag, take_until, take_while_m_n},
     combinator::{peek, recognize},
     error::{context, ContextError, ParseError},
     multi::many1_count,
     sequence::{preceded, terminated, tuple},
     IResult,
 };
+use nom::branch::alt;
+use nom::character::is_digit;
+use gix_actor::{Sign, SignatureRef, Time};
 
 use crate::ByteSlice;
 
@@ -76,6 +80,51 @@ pub fn hex_hash<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], &
 
 pub(crate) fn signature<'a, E: ParseError<&'a [u8]> + ContextError<&'a [u8]>>(
     i: &'a [u8],
-) -> IResult<&'a [u8], gix_actor::SignatureRef<'a>, E> {
-    gix_actor::signature::decode(i)
+) -> IResult<&'a [u8], SignatureRef<'a>, E> {
+    let (i, (name, email, time, tzsign, hours, minutes)) = context(
+        "<name> <<email>> <timestamp> <+|-><HHMM>",
+        tuple((
+            context("<name>", terminated(take_until(&b" <"[..]), take(2usize))),
+            context("<email>", terminated(take_until(&b"> "[..]), take(2usize))),
+            context("<timestamp>", |i| {
+                terminated(take_until(SPACE), take(1usize))(i).and_then(|(i, v)| {
+                    btoi::<u32>(v)
+                        .map(|v| (i, v))
+                        .map_err(|_| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
+                })
+            }),
+            context("+|-", alt((tag(b"-"), tag(b"+")))),
+            context("HH", |i| {
+                take_while_m_n(2usize, 2, is_digit)(i).and_then(|(i, v)| {
+                    btoi::<i32>(v)
+                        .map(|v| (i, v))
+                        .map_err(|_| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
+                })
+            }),
+            context("MM", |i| {
+                take_while_m_n(2usize, 2, is_digit)(i).and_then(|(i, v)| {
+                    btoi::<i32>(v)
+                        .map(|v| (i, v))
+                        .map_err(|_| nom::Err::Error(E::from_error_kind(i, nom::error::ErrorKind::MapRes)))
+                })
+            }),
+        )),
+    )(i)?;
+
+    debug_assert!(tzsign[0] == b'-' || tzsign[0] == b'+', "parser assure it's +|- only");
+    let sign = if tzsign[0] == b'-' { Sign::Minus } else { Sign::Plus }; //
+    let offset = (hours * 3600 + minutes * 60) * if sign == Sign::Minus { -1 } else { 1 };
+
+    Ok((
+        i,
+        SignatureRef {
+            name: name.as_bstr(),
+            email: email.as_bstr(),
+            time: Time {
+                seconds_since_unix_epoch: time,
+                offset_in_seconds: offset,
+                sign,
+            },
+        },
+    ))
 }
