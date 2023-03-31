e3d04f7305f42385f489e9a3521fc1c9d93485a1
Sewen Thy <me@sewenthy.dev>
2023-03-31 18:15:02 +0800

    ext7-expr-active

--- gix-glob/src/parse.rs
+++ gix-glob/src/parse.rs
@@ -1,13 +1,11 @@
-use std::slice::Iter;
-use bstr::{BString, ByteSlice};
-
 use crate::{pattern, pattern::Mode};
-
+use bstr::{BString, ByteSlice};
+use std::slice::Iter;
 #[inline]
-/// A sloppy parser that performs only the most basic checks, providing additional information
-/// using `pattern::Mode` flags.
-///
-/// Returns `(pattern, mode, no_wildcard_len)`
+#[doc = " A sloppy parser that performs only the most basic checks, providing additional information"]
+#[doc = " using `pattern::Mode` flags."]
+#[doc = ""]
+#[doc = " Returns `(pattern, mode, no_wildcard_len)`"]
 pub fn pattern(mut pat: &[u8]) -> Option<(BString, pattern::Mode, Option<usize>)> {
     let mut mode = Mode::empty();
     if pat.is_empty() {
@@ -34,54 +32,43 @@ pub fn pattern(mut pat: &[u8]) -> Option<(BString, pattern::Mode, Option<usize>)
         mode |= Mode::MUST_BE_DIR;
         pat.pop();
     }
-
     if !pat.contains(&b'/') {
         mode |= Mode::NO_SUB_DIR;
     }
     if pat.first() == Some(&b'*') && first_wildcard_pos(&pat[1..]).is_none() {
         mode |= Mode::ENDS_WITH;
     }
-
     let pos_of_first_wildcard = first_wildcard_pos(&pat);
     Some((pat, mode, pos_of_first_wildcard))
 }
-
 fn first_wildcard_pos(pat: &[u8]) -> Option<usize> {
     pat.find_byteset(GLOB_CHARACTERS)
 }
-
 pub(crate) const GLOB_CHARACTERS: &[u8] = br"*?[\";
-
-/// We always copy just because that's ultimately needed anyway, not because we always have to.
+#[doc = " We always copy just because that's ultimately needed anyway, not because we always have to."]
 fn truncate_non_escaped_trailing_spaces(buf: &[u8]) -> BString {
     match buf.rfind_not_byteset(br"\ ") {
-        Some(pos) if pos + 1 == buf.len() => buf.into(), // does not end in (escaped) whitespace
+        Some(pos) if pos + 1 == buf.len() => buf.into(),
         None => buf.into(),
         Some(start_of_non_space) => {
-            // This seems a bit strange but attempts to recreate the git implementation while
-            // actually removing the escape characters before spaces. We leave other backslashes
-            // for escapes to be handled by `glob/globset`.
             let mut res: BString = buf[..start_of_non_space + 1].into();
-
             let mut trailing_bytes = buf[start_of_non_space + 1..].iter();
             let mut bare_spaces = 0;
             while let Some(b) = trailing_bytes.next() {
-                bar____EXTRACT_THIS(res, trailing_bytes, bare_spaces, b)
+                bar(&mut res, &mut trailing_bytes, &mut bare_spaces, b)
             }
             res
         }
     }
 }
-
-fn bar____EXTRACT_THIS(res: BString, trailing_bytes: Iter<'_, u8>, bare_spaces: usize, b: &'a u8) {
+fn bar(res: &mut BString, trailing_bytes: &mut Iter<'_, u8>, bare_spaces: &mut usize, b: &u8) {
     match b {
         b' ' => {
-            bare_spaces += 1;
+            (*bare_spaces) += 1;
         }
         b'\\' => {
-            res.extend(std::iter::repeat(b' ').take(bare_spaces));
-            bare_spaces = 0;
-            // Skip what follows, like git does, but keep spaces if possible.
+            res.extend(std::iter::repeat(b' ').take((*bare_spaces)));
+            (*bare_spaces) = 0;
             if trailing_bytes.next() == Some(&b' ') {
                 res.push(b' ');
             }
