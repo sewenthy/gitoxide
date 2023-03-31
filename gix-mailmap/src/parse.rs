b42a8bebba01bdcda5201ce2942e02b88e85d55c
Sewen Thy <me@sewenthy.dev>
2023-03-31 18:14:16 +0800

    ext2-expr-active

--- gix-mailmap/src/parse.rs
+++ gix-mailmap/src/parse.rs
@@ -1,33 +1,16 @@
 mod error {
     use bstr::BString;
     use quick_error::quick_error;
-
-    quick_error! {
-        /// The error returned by [`parse()`][crate::parse()].
-        #[derive(Debug)]
-        #[allow(missing_docs)]
-        pub enum Error {
-            UnconsumedInput { line_number: usize, line: BString } {
-                display("Line {} has too many names or emails, or none at all: {}", line_number, line)
-            }
-            Malformed { line_number: usize, line: BString, message: String } {
-                display("{}: {:?}: {}", line_number, line, message)
-            }
-        }
-    }
+    quick_error! { # [doc = " The error returned by [`parse()`][crate::parse()]."] # [derive (Debug)] # [allow (missing_docs)] pub enum Error { UnconsumedInput { line_number : usize , line : BString } { display ("Line {} has too many names or emails, or none at all: {}" , line_number , line) } Malformed { line_number : usize , line : BString , message : String } { display ("{}: {:?}: {}" , line_number , line , message) } } }
 }
-
+use crate::Entry;
 use bstr::{BStr, ByteSlice};
 pub use error::Error;
-
-use crate::Entry;
-
-/// An iterator to parse mailmap lines on-demand.
+#[doc = " An iterator to parse mailmap lines on-demand."]
 pub struct Lines<'a> {
     lines: bstr::Lines<'a>,
     line_no: usize,
 }
-
 impl<'a> Lines<'a> {
     pub(crate) fn new(input: &'a [u8]) -> Self {
         Lines {
@@ -36,10 +19,8 @@ impl<'a> Lines<'a> {
         }
     }
 }
-
 impl<'a> Iterator for Lines<'a> {
     type Item = Result<Entry<'a>, Error>;
-
     fn next(&mut self) -> Option<Self::Item> {
         for line in self.lines.by_ref() {
             self.line_no += 1;
@@ -57,7 +38,6 @@ impl<'a> Iterator for Lines<'a> {
         None
     }
 }
-
 fn parse_line(line: &BStr, line_number: usize) -> Result<Entry<'_>, Error> {
     let (name1, email1, rest) = parse_name_and_email(line, line_number)?;
     let (name2, email2, rest) = parse_name_and_email(rest, line_number)?;
@@ -67,12 +47,29 @@ fn parse_line(line: &BStr, line_number: usize) -> Result<Entry<'_>, Error> {
             line: line.into(),
         });
     }
-    Ok(bar____EXTRACT_THIS(line, line_number, name1, email1, name2, email2))
+    Ok(match bar(line, line_number, name1, email1, name2, email2) {
+        RetBar::Ok(x) => x,
+        RetBar::Return(x) => return x,
+    })
 }
-
-fn bar____EXTRACT_THIS(line: &BStr, line_number: usize, name1: Option<&BStr>, email1: Option<&BStr>, name2: Option<&BStr>, email2: Option<&BStr>) -> Entry<'a> {
-    match (name1, email1, name2, email2) {
-        (Some(proper_name), Some(commit_email), None, None) => Entry::change_name_by_email(proper_name, commit_email),
+fn bar<'lt0, 'lt1, 'lt2, 'lt3, 'lt4, 'lt5>(
+    line: &BStr,
+    line_number: usize,
+    name1: Option<&'lt0 BStr>,
+    email1: Option<&'lt1 BStr>,
+    name2: Option<&'lt2 BStr>,
+    email2: Option<&'lt3 BStr>,
+) -> RetBar<Entry<'lt4>, Result<Entry<'lt5>, Error>>
+where
+    'lt0: 'lt4,
+    'lt1: 'lt4,
+    'lt2: 'lt4,
+    'lt3: 'lt4,
+{
+    let result = match (name1, email1, name2, email2) {
+        (Some(proper_name), Some(commit_email), None, None) => {
+            Entry::change_name_by_email(proper_name, commit_email)
+        }
         (None, Some(proper_email), None, Some(commit_email)) => {
             Entry::change_email_by_email(proper_email, commit_email)
         }
@@ -80,18 +77,23 @@ fn bar____EXTRACT_THIS(line: &BStr, line_number: usize, name1: Option<&BStr>, em
             Entry::change_name_and_email_by_email(proper_name, proper_email, commit_email)
         }
         (Some(proper_name), Some(proper_email), Some(commit_name), Some(commit_email)) => {
-            Entry::change_name_and_email_by_name_and_email(proper_name, proper_email, commit_name, commit_email)
+            Entry::change_name_and_email_by_name_and_email(
+                proper_name,
+                proper_email,
+                commit_name,
+                commit_email,
+            )
         }
         _ => {
-            return Err(Error::Malformed {
+            return RetBar::Return(Err(Error::Malformed {
                 line_number,
                 line: line.into(),
                 message: "Emails without a name or email to map to are invalid".into(),
-            })
+            }))
         }
-    }
+    };
+    RetBar::Ok(result)
 }
-
 fn parse_name_and_email(
     line: &BStr,
     line_number: usize,
@@ -118,4 +120,8 @@ fn parse_name_and_email(
         }
         None => Ok((None, None, line)),
     }
+}
+enum RetBar<A, B> {
+    Ok(A),
+    Return(B),
 }
