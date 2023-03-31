9ebabb64a8aac31f9d4145df0639704dec471724
Sewen Thy <me@sewenthy.dev>
2023-03-31 18:16:24 +0800

    inline-ext6-expr-active

--- gix-discover/src/is.rs
+++ gix-discover/src/is.rs
@@ -1,19 +1,15 @@
-use std::{borrow::Cow, ffi::OsStr, path::Path};
-
 use crate::{DOT_GIT_DIR, MODULES};
-
-/// Returns true if the given `git_dir` seems to be a bare repository.
-///
-/// Please note that repositories without an index generally _look_ bare, even though they might also be uninitialized.
+use std::{borrow::Cow, ffi::OsStr, path::Path};
+#[doc = " Returns true if the given `git_dir` seems to be a bare repository."]
+#[doc = ""]
+#[doc = " Please note that repositories without an index generally _look_ bare, even though they might also be uninitialized."]
 pub fn bare(git_dir_candidate: impl AsRef<Path>) -> bool {
     let git_dir = git_dir_candidate.as_ref();
     !(git_dir.join("index").exists() || (git_dir.file_name() == Some(OsStr::new(DOT_GIT_DIR))))
 }
-
-/// Returns true if `git_dir` is is located within a `.git/modules` directory, indicating it's a submodule clone.
+#[doc = " Returns true if `git_dir` is is located within a `.git/modules` directory, indicating it's a submodule clone."]
 pub fn submodule_git_dir(git_dir: impl AsRef<Path>) -> bool {
     let git_dir = git_dir.as_ref();
-
     let mut last_comp = None;
     git_dir.file_name() != Some(OsStr::new(DOT_GIT_DIR))
         && git_dir.components().rev().any(|c| {
@@ -26,18 +22,17 @@ pub fn submodule_git_dir(git_dir: impl AsRef<Path>) -> bool {
         })
         && last_comp == Some(OsStr::new(MODULES))
 }
-
-/// What constitutes a valid git repository, returning the guessed repository kind
-/// purely based on the presence of files. Note that the gix-config ultimately decides what's bare.
-///
-/// Returns the `Kind` of git directory that was passed, possibly alongside the supporting private worktree git dir.
-///
-/// Note that `.git` files are followed to a valid git directory, which then requires…
-///
-///   * …a valid head
-///   * …an objects directory
-///   * …a refs directory
-///
+#[doc = " What constitutes a valid git repository, returning the guessed repository kind"]
+#[doc = " purely based on the presence of files. Note that the gix-config ultimately decides what's bare."]
+#[doc = ""]
+#[doc = " Returns the `Kind` of git directory that was passed, possibly alongside the supporting private worktree git dir."]
+#[doc = ""]
+#[doc = " Note that `.git` files are followed to a valid git directory, which then requires…"]
+#[doc = ""]
+#[doc = "   * …a valid head"]
+#[doc = "   * …an objects directory"]
+#[doc = "   * …a refs directory"]
+#[doc = ""]
 pub fn git(git_dir: impl AsRef<Path>) -> Result<crate::repository::Kind, crate::is_git::Error> {
     #[derive(Eq, PartialEq)]
     enum Kind {
@@ -85,7 +80,9 @@ pub fn git(git_dir: impl AsRef<Path>) -> Result<crate::repository::Kind, crate::
             .and_then(|cd| {
                 crate::path::from_plain_file(git_dir.join("gitdir"))
                     .and_then(Result::ok)
-                    .map(|worktree_gitfile| (crate::path::without_dot_git_dir(worktree_gitfile), cd))
+                    .map(|worktree_gitfile| {
+                        (crate::path::without_dot_git_dir(worktree_gitfile), cd)
+                    })
             });
         match worktree_and_common_dir {
             Some((work_dir, common_dir)) => {
@@ -96,14 +93,14 @@ pub fn git(git_dir: impl AsRef<Path>) -> Result<crate::repository::Kind, crate::
                     Kind::WorkTreeGitDir { work_dir },
                 )
             }
-            None => (Cow::Borrowed(git_dir), Cow::Borrowed(git_dir), Kind::MaybeRepo),
+            None => (
+                Cow::Borrowed(git_dir),
+                Cow::Borrowed(git_dir),
+                Kind::MaybeRepo,
+            ),
         }
     };
-
     {
-        // We expect to be able to parse any ref-hash, so we shouldn't have to know the repos hash here.
-        // With ref-table, the has is probably stored as part of the ref-db itself, so we can handle it from there.
-        // In other words, it's important not to fail on detached heads here because we guessed the hash kind wrongly.
         let object_hash_should_not_matter_here = gix_hash::Kind::Sha1;
         let refs = gix_ref::file::Store::at(
             dot_git.as_ref(),
@@ -117,11 +114,12 @@ pub fn git(git_dir: impl AsRef<Path>) -> Result<crate::repository::Kind, crate::
             });
         }
     }
-
     {
         let objects_path = common_dir.join("objects");
         if !objects_path.is_dir() {
-            return Err(crate::is_git::Error::MissingObjectsDirectory { missing: objects_path });
+            return Err(crate::is_git::Error::MissingObjectsDirectory {
+                missing: objects_path,
+            });
         }
     }
     {
@@ -142,28 +140,29 @@ pub fn git(git_dir: impl AsRef<Path>) -> Result<crate::repository::Kind, crate::
             if bare(git_dir) {
                 crate::repository::Kind::Bare
             } else {
-                let mut is_submodule_git_dir = bar____EXTRACT_THIS(git_dir);
+                let mut is_submodule_git_dir = bar(git_dir);
                 if is_submodule_git_dir {
                     crate::repository::Kind::SubmoduleGitDir
                 } else {
-                    crate::repository::Kind::WorkTree { linked_git_dir: None }
+                    crate::repository::Kind::WorkTree {
+                        linked_git_dir: None,
+                    }
                 }
             }
         }
     })
 }
-
-fn bar____EXTRACT_THIS(git_dir: &Path) -> bool {
+fn bar(git_dir: &Path) -> bool {
     let mut last_comp = None;
     let is_submodule_git_dir = git_dir.file_name() != Some(OsStr::new(DOT_GIT_DIR))
         && git_dir.components().rev().any(|c| {
-        if c.as_os_str() == OsStr::new(DOT_GIT_DIR) {
-            true
-        } else {
-            last_comp = Some(c.as_os_str());
-            false
-        }
-    })
+            if c.as_os_str() == OsStr::new(DOT_GIT_DIR) {
+                true
+            } else {
+                last_comp = Some(c.as_os_str());
+                false
+            }
+        })
         && last_comp == Some(OsStr::new(MODULES));
     is_submodule_git_dir
 }
