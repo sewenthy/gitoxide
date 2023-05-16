05e910009bf5ffd51efc8474e84ba571054643b7
Sewen Thy <me@sewenthy.dev>
2023-05-16 11:40:30 +0800

    add start end selection

--- gix-config/src/source.rs
+++ gix-config/src/source.rs
@@ -24,13 +24,13 @@ pub enum Kind {
 impl Kind {
     /// Return a list of sources associated with this `Kind` of source, in order of ascending precedence.
     pub fn sources(self) -> &'static [Source] {
-        let src = match self {
+        let src = /* START SELECTION */ match self {
             Kind::GitInstallation => &[Source::GitInstallation] as &[_],
             Kind::System => &[Source::System],
             Kind::Global => &[Source::Git, Source::User],
             Kind::Repository => &[Source::Local, Source::Worktree],
             Kind::Override => &[Source::Env, Source::Cli, Source::Api],
-        };
+        }/* END SELECTION */;
         debug_assert!(
             src.iter().all(|src| src.kind() == self),
             "BUG: classification of source has to match the ordering here, see `Source::kind()`"
